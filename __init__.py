52866eb465b2e4a68d4a45a029607601c73a2882
IDKHowFoundMe <emoreno.cristina@gmail.com>
2026-01-13 21:50:53 -0500

    Subida Taller Descomposicion Lu

--- __init__.py
+++
@@ -1,2 +0,0 @@
-from .linear_syst_methods import gaussJordan,eliminacion_gaussiana2, descomposicion_LU, resolver_LU, matriz_aumentada, separar_m_aumentada   # type: ignore
-from .min_cuadrados import ajustar_min_cuadrados  # type: ignore
